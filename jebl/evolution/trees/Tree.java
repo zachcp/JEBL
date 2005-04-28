0c8354fb2da08eb71bccc66802b7d3d8631b6132
Alexei Drummond <alexei.drummond@gmail.com>
2005-04-28 17:24:57 +0000

    no message
    git-svn-id: http://svn.code.sf.net/p/jebl/code/trunk@8 6721810a-1124-0410-938e-bb277542c816

--- jebl/evolution/trees/Tree.java
+++ jebl/evolution/trees/Tree.java
@@ -1,7 +1,5 @@
 /*
- * Tree.java
- *
- * (c) 2002-2005 BEAST Development Core Team
+ * (c) 2002-2005 JEBL Development Core Team
  *
  * This package may be distributed under the
  * Lesser Gnu Public Licence (LGPL)
@@ -15,20 +13,51 @@ import jebl.evolution.graphs.Node;
 import java.util.Set;
 
 /**
+ * An unrooted tree.
+ *
  * @author rambaut
- *         Date: Apr 6, 2005
- *         Time: 12:06:25 PM
+ * @author Alexei Drummond
+ *
+ * @version $Id$
  */
 public interface Tree extends Graph {
 
-    Set getInternalNodes();
+    /**
+     * @return a set of all nodes that have degree 1.
+     * These nodes are often refered to as 'tips'.
+     */
     Set getExternalNodes();
 
-    boolean isExternal(Node node);
+    /**
+     * @return a set of all nodes that have degree 2 or more.
+     * These nodes are often refered to as internal nodes.
+     */
+    Set getInternalNodes();
 
-    Taxon getTaxon(Node node);
+    /**
+     * @return the set of taxa associated with the external
+     * nodes of this tree. The size of this set should be the
+     * same as the size of the external nodes set.
+     */
     Set getTaxa();
 
-    Node getNode(Taxon taxon);
+    /**
+     * @param node the node whose associated taxon is being requested.
+     * @return the taxon object associated with the given node, or null
+     * if the node is an internal node.
+     */
+    Taxon getTaxon(Node node);
 
+    /**
+     * @param node the node
+     * @return true if the node is of degree 1.
+     */
+    boolean isExternal(Node node);
+
+    /**
+     * @param taxon the taxon
+     * @return the external node associated with the given taxon, or null
+     * if the taxon is not a member of the taxa set associated with this tree.
+     */
+    Node getNode(Taxon taxon);
 }
