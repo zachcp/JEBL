18e800fe8aad8e5e0cdc5dac5d85fbfac74a5fa8
Alexei Drummond <alexei.drummond@gmail.com>
2005-04-28 17:18:08 +0000

    no message
    git-svn-id: http://svn.code.sf.net/p/jebl/code/trunk@7 6721810a-1124-0410-938e-bb277542c816

--- jebl/evolution/trees/RootedTree.java
+++ jebl/evolution/trees/RootedTree.java
@@ -1,31 +1,53 @@
 /*
- * RootedTree.java
+ * (c) 2002-2005 JEBL Development Core Team
  *
- * (c) 2002-2005 BEAST Development Core Team
- *
- * This package may be distributed under the
+ * This package is distributed under the
  * Lesser Gnu Public Licence (LGPL)
  */
 package jebl.evolution.trees;
 
-import jebl.evolution.taxa.Taxon;
-import jebl.evolution.graphs.Graph;
 import jebl.evolution.graphs.Node;
 
 import java.util.Set;
 
 /**
+ * A tree with a root (node with maximum height). This interface
+ * provides the concept of a direction of time that flows from the
+ * root to the tips. Each node in the tree has a node height that is
+ * less than its parent's height and greater than it children's heights.
+ *
  * @author rambaut
- *         Date: Apr 6, 2005
- *         Time: 12:06:25 PM
+ * @author Alexei Drummond
+ *
+ * @version $Id$
  */
 public interface RootedTree extends Tree {
 
-    Node getRootNode();
-
+    /**
+     * @param node the node whose children are being requested.
+     * @return the set of nodes that are the children of the given node.
+     * The set may be empty for a terminal node (a tip).
+     */
     Set getChildren(Node node);
 
+    /**
+     * @param node the node whose height is being requested.
+     * @return the height of the given node. The height will be
+     * less than the parent's height and greater than it children's heights.
+     */
     double getNodeHeight(Node node);
 
+    /**
+     * @param node the node whose parent is requested
+     * @return the parent node of the given node, or null
+     * if the node is the root node.
+     */
+    Node getParent(Node node);
 
+    /**
+     * The root of the tree has the largest node height of
+     * all nodes in the tree.
+     * @return the root of the tree.
+     */
+    Node getRootNode();
 }
