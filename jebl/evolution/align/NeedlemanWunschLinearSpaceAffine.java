8f32fde1f141c0852b0a8a351e6a26979c50c9f7
Matt Kearse <matt@biomatters.com>
2005-08-26 02:55:16 +0000

    no message
    git-svn-id: http://svn.code.sf.net/p/jebl/code/trunk@62 6721810a-1124-0410-938e-bb277542c816

---
+++ jebl/evolution/align/NeedlemanWunschLinearSpaceAffine.java
@@ -0,0 +1,193 @@
+package jebl.evolution.align;
+// Do not use yet. Implementation not complete !!!
+
+import jebl.evolution.align.scores.Scores;
+
+// Global alignment using the Needleman-Wunsch algorithm (affine gap costs)
+// uses linear space.
+
+public class NeedlemanWunschLinearSpaceAffine extends AlignLinearSpaceAffine {
+
+	public NeedlemanWunschLinearSpaceAffine(Scores sub, float d, float e) {
+		super(sub, d, e);
+	}
+
+    String[] matchResult;
+    private static final int TYPE_ANY = 0;
+    private static final int TYPE_X = 1;
+    private static final int TYPE_Y= 2;
+    private int C[][][];
+    private int Ctype[][][];
+
+    /**
+	 * @param sq1
+	 * @param sq2
+	 */
+
+    public void doAlignment(String sq1, String sq2) {
+        sq1 = strip(sq1);
+        sq2 = strip(sq2);
+
+        prepareAlignment (sq1,sq2);
+        //we initialise the following arrays here rather than in prepareAlignment
+        //so that we do not have to create them again during recursion.
+        F = new float[3][2][m + 1];
+        C = new int [3] [ 2] [m+1];
+        Ctype = new int [3] [3] [m+1];
+        matchResult =doAlignment (sq1,sq2, TYPE_ANY, TYPE_ANY);
+    }
+
+    public void prepareAlignment(String sq1, String sq2) {
+        this.seq1 = sq1;
+        this.seq2 = sq2;
+
+        this.n = sq1.length();
+        this.m = sq2.length();
+
+    }
+
+    private String[] doAlignment(String sq1, String sq2, int startType, int endType) {
+        prepareAlignment(sq1, sq2);
+
+        char[] s1 = sq1.toCharArray();
+        char[] s2 = sq2.toCharArray();
+
+        int n = this.n, m = this.m;
+        float[][] score = sub.score;
+        float[][] M = F[0], Ix = F[1], Iy = F[2];
+        int[][] cm= C [0], cx= C [1],cy= C [2];
+        int[][] cmtype= Ctype [0], cxtype= Ctype [1],cytype= Ctype [2];
+        float val;
+        float s, a, b, c;
+        int u=n/2;
+
+        if (n< 6 || m<6) {
+            NeedlemanWunschAffine align = new NeedlemanWunschAffine (sub,d,e);
+            align.doAlignment(sq1,sq2);
+            return align.getMatch();
+        }
+
+        //all that the remainder of this function does is to calculate the midpoint
+        //(u,v) that the optimal alignment passes through, along with the
+        //type of extension applied at the midpoint, in case it is the gap extension,
+        //in which case the recursion functions need to know that.
+
+        for (int j = 1; j <= m; j++) {
+            float base = d;
+            if (startType ==TYPE_Y)
+                base =e;//if startType IS TYPE_Y then we were already in a
+            // gap, so we can use gap extension penalty rather than gap starting penalty
+            Iy[0] [j] = - base -e*(j- 1);
+            Ix [0] [j] = M [0] [j] = Float.NEGATIVE_INFINITY;
+        }
+        Ix[0][0] = Iy[0][0] = M[0][0]= 0;
+        swap01(Ix);swap01(Iy); swap01 (M);
+        swap01(cm); swap01 (cx); swap01 (cy);
+        swap01(cmtype); swap01 (cxtype); swap01 (cytype);
+
+        for (int i=1; i<=n; i++) {
+            swap01(Ix);swap01(Iy); swap01 (M);
+            swap01(cm); swap01 (cx); swap01 (cy);
+            swap01(cmtype); swap01 (cxtype); swap01 (cytype);
+
+            M[1][0] = Float.NEGATIVE_INFINITY;
+            Iy[1][0] = Float.NEGATIVE_INFINITY;
+            float base =d;
+            if (startType == TYPE_X) base =e;
+            Ix[1][0] = - base -e*(i- 1);
+            for (int j=1; j<=m; j++) {
+                s = score[s1[i-1]][s2[j-1]];
+                a = M[0][j-1]+s;
+                b = Ix[0][j-1]+s;
+                c = Iy[0][j-1]+s;
+
+                val = M[i][j] = max(a, b, c);
+                if (i == u) {
+                    cm[1][j]=j;
+                    cmtype[1][j]= TYPE_ANY;
+                }
+                else if (i > u) {
+                    if (val == a) {
+                        cm[1][j]= cm[0][j-1];
+                        cmtype[1][j]= cmtype[0][j-1];
+                    }
+                    else if (val == b) {
+                        cm[1][j]= cx[0][j-1];
+                        cmtype[1][j]= cxtype[0][j-1];
+                    }
+                    else if (val == c) {
+                        cm[1][j]= cy[0][j-1];
+                        cmtype[1][j]=cytype[0][j-1];
+                    } else {
+                        throw new Error("NWAffine 1");
+                    }
+                }
+
+                a = M[i-1][j]-d;
+                b = Ix[i-1][j]-e;
+                c = Iy[i-1][j]-d;
+                val = Ix[i][j] = max(a, b, c);
+                if (i == u) {
+                    cx[1][j] = j;
+                    cxtype[1][j] = TYPE_X;
+                } else if (i > u) {
+                    if (val == a) {
+                        cx[1][j] = cm[0][j];
+                        cxtype[1][j] = cmtype[0][j];
+                    } else if (val == b) {
+                        cx[1][j] = cx[0][j];
+                        cxtype[1][j] = cxtype[0][j];
+                    } else if (val == c) {
+                        cx[1][j] = cy[0][j];
+                        cxtype[1][j] = cytype[0][j];
+                    } else {
+                        throw new Error("NWAffine 2");
+                    }
+                }
+
+
+                a = M[i][j-1]-d;
+                b = Iy[i][j-1]-e;
+                c = Ix[i][j-1]-d;
+                val = Iy[i][j] = max(a, b, c);
+                if (i == u) {
+                    cm[1][j] = j;
+                    cmtype[1][j] = TYPE_ANY;
+                } else if (i > u) {
+                    if (val == a) {
+                        cy[1][j] = cm[1][j - 1];
+                        cytype[1][j] = cmtype[1][j - 1];
+                    } else if (val == b) {
+                        cy[1][j] = cx[1][j - 1];
+                        cytype[1][j] = cxtype[1][j - 1];
+                    } else if (val == c) {
+                        cy[1][j] = cy[1][j - 1];
+                        cytype[1][j] = cytype[1][j - 1];
+                    } else {
+                        throw new Error("NWAffine 3");
+                    }
+                }
+
+            }
+        }
+        // Find maximal score
+        int bestk = 0;
+        for (int k = 1; k < 3; k++) {
+            if (F[k][1][m]> F [ bestk][1][m])
+                bestk=k;
+        }
+
+        //if the alignment must end with a particular type, force that type to be selected:
+        if (endType == TYPE_X)
+            bestk= 1;
+        if (endType == TYPE_Y)
+            bestk= 2;
+
+        int v= C [ bestk][1][m];
+        int vtype = Ctype [ bestk][1][m];
+
+        String[] match1= doAlignment(sq1.substring(0,u),sq2.substring(0,v),startType,vtype );
+        String[] match2= doAlignment(sq1.substring(u),sq2.substring(v),vtype, endType );
+        return new String[] {match1[0]+ match2[0], match1[1]+ match2[1]};
+    }
+}
