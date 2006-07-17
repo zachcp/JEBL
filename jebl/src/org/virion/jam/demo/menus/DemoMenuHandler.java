3b1e8259ba9785558adae47ee876a5c403abcb0f
Andrew Rambaut <a.rambaut@ed.ac.uk>
2006-07-17 21:35:49 +0000

    no message
    git-svn-id: http://svn.code.sf.net/p/jebl/code/trunk@386 6721810a-1124-0410-938e-bb277542c816

---
+++ jebl/src/org/virion/jam/demo/menus/DemoMenuHandler.java
@@ -0,0 +1,14 @@
+package org.virion.jam.demo.menus;
+
+import javax.swing.*;
+
+/**
+ * @author Andrew Rambaut
+ * @version $Id$
+ */
+public interface DemoMenuHandler {
+	Action getFirstAction();
+	Action getSecondAction();
+
+
+}
