9b5fb4ebbeb042b32c8f3103f128a7480c09346e
Matt Kearse <matt@biomatters.com>
2008-06-18 05:58:28 +0000

    refactoring for GEN-5084 Similariry graph: ambiguities should be handled.
    Move method to its parent class.
    
    git-svn-id: http://svn.code.sf.net/p/jebl/code/trunk@924 6721810a-1124-0410-938e-bb277542c816

--- jebl/src/jebl/evolution/sequences/State.java
+++ jebl/src/jebl/evolution/sequences/State.java
@@ -81,4 +81,33 @@ public abstract class State implements Comparable {
     private String name;
     private Set<State> ambiguities;
     private int index;
+
+    /**
+     * Determine how much in common these potentially ambigous states have as a fraction between 0 and 1
+     * 2 non-ambiguous states will return 0.
+     * 2 identical non-ambigoues states will 1.
+     * e.g. for Nucleotides
+     * R,A = 0.5
+     * R,G = 0.5
+     * R,M = 0.25
+     * @param other another state to compare with
+     * @return the fraction of canonical states that the 2 potentially ambiguous states have in common between 0 and 1.
+     */
+    public double fractionEqual(State other) {
+        int totalStates= 0;
+        int sameStates = 0;
+        if (isGap() || other.isGap()) {
+            return 0;
+        }
+        for (State state : getCanonicalStates()) {
+            for (State state1 : other.getCanonicalStates()) {
+                totalStates++;
+                if (state.equals(state1)) {
+                    sameStates++;
+                }
+            }
+        }
+        return ((double)sameStates)/totalStates;
+    }
+
 }
