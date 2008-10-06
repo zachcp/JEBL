c0b1f74e501ead185d50c7a8fcc357a90f89df3b
Matt Kearse <matt@biomatters.com>
2008-10-06 00:36:32 +0000

    Introducing "BasicProgressListener"
    git-svn-id: http://svn.code.sf.net/p/jebl/code/trunk@949 6721810a-1124-0410-938e-bb277542c816

---
+++ jebl/src/jebl/util/BasicProgressListener.java
@@ -0,0 +1,51 @@
+package jebl.util;
+
+/**
+ * A basic {@link jebl.util.ProgressListener} implementation that allows the caller to set the canceled
+ * status. Typically this class is used when you don't care about providing immediate feedback to
+ * the user, but still need to be able to cancel an operation.
+ * @author Matt Kearse
+ * @version $Id$
+ */
+
+public class BasicProgressListener extends ProgressListener {
+    private boolean canceled=false;
+    private String message="";
+    private double fractionCompleted = 0;
+    protected void _setProgress(double fractionCompleted) {
+        this.fractionCompleted = fractionCompleted;
+    }
+
+    protected void _setIndeterminateProgress() {
+    }
+
+    protected void _setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isCanceled() {
+        return canceled;
+    }
+
+    /**
+     * Sets this progress listener as cancel, so that {@link #isCanceled()} will return true.
+     */
+    public void cancel() {
+        this.canceled = true;
+    }
+
+    /**
+     *
+     * @return the most recent message set on this progress listener.
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * @return the current fraction complete.
+     */
+    public double getFractionCompleted() {
+        return fractionCompleted;
+    }
+}
