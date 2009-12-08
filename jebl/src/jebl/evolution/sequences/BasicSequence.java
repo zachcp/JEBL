2d2f491b0fa4623fc8165009226cd575a3ec0569
Amy Wilson <amy@biomatters.com>
2009-12-08 00:14:20 +0000

    GEN-9099 Some fixes (more to come)
    git-svn-id: http://svn.code.sf.net/p/jebl/code/trunk@1042 6721810a-1124-0410-938e-bb277542c816

--- jebl/src/jebl/evolution/sequences/BasicSequence.java
+++ jebl/src/jebl/evolution/sequences/BasicSequence.java
@@ -11,6 +11,7 @@ package jebl.evolution.sequences;
 import jebl.evolution.taxa.Taxon;
 import jebl.util.AttributableHelper;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
@@ -215,4 +216,25 @@ public class BasicSequence implements Sequence {
     private final byte[] sequenceCharacters; // this is really an array of characters, but using bytes since we don't store high-ascii characters
 
    // private Map<String, Object> attributeMap = null;
+   @Override
+   public boolean equals(Object o) {
+       if (this == o) return true;
+       if (o == null || getClass() != o.getClass()) return false;
+
+       BasicSequence that = (BasicSequence) o;
+
+       if (!Arrays.equals(sequenceCharacters, that.sequenceCharacters)) return false;
+       if (sequenceType != null ? !sequenceType.equals(that.sequenceType) : that.sequenceType != null) return false;
+       if (taxon != null ? !taxon.equals(that.taxon) : that.taxon != null) return false;
+
+       return true;
+   }
+
+    @Override
+    public int hashCode() {
+        int result = (taxon != null ? taxon.hashCode() : 0);
+        result = 31 * result + (sequenceType != null ? sequenceType.hashCode() : 0);
+        result = 31 * result + (sequenceCharacters != null ? Arrays.hashCode(sequenceCharacters) : 0);
+        return result;
+    }
 }
