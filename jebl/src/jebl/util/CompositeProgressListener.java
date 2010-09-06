297d787a911acd9b3fa080d187dee474c238353f
Andrew Rambaut <a.rambaut@ed.ac.uk>
2010-09-06 19:57:34 +0000

    Removed a dependency between JEBL and the UI code in JAM by moving SimpleListener into jebl.util
    git-svn-id: http://svn.code.sf.net/p/jebl/code/trunk@1067 6721810a-1124-0410-938e-bb277542c816

--- jebl/src/jebl/util/CompositeProgressListener.java
+++ jebl/src/jebl/util/CompositeProgressListener.java
@@ -1,7 +1,6 @@
 package jebl.util;
 
 import jebl.math.MachineAccuracy;
-import org.virion.jam.util.SimpleListener;
 
 import java.awt.*;
 import java.io.File;
---
+++ jebl/src/jebl/util/SimpleListener.java
@@ -0,0 +1,10 @@
+package jebl.util;
+
+/**
+ * @author Richard Moir
+ * @version $Id: SimpleListener.java 965 2009-01-03 00:39:29Z rambaut $
+ */
+public interface SimpleListener {
+
+    void objectChanged();
+}
