aa6abbf7a888c294d30f9c93539470d19d9918b3
Richard Moir <richard@biomatters.com>
2013-09-13 00:01:16 +0000

    GEN-21155 Explore and document the licensing code
    Add an EMPTY SimpleListener constant.
    
    Written with Amy
    
    git-svn-id: http://svn.code.sf.net/p/jebl/code/trunk@1164 6721810a-1124-0410-938e-bb277542c816

--- jebl/src/org/virion/jam/util/SimpleListener.java
+++ jebl/src/org/virion/jam/util/SimpleListener.java
@@ -16,4 +16,17 @@ package org.virion.jam.util;
 public interface SimpleListener {
 
     void objectChanged();
+
+    /**
+     * A SimpleListener that does nothing when objectChanged is called. Useful when a method has a SimpleListener parameter
+     * but you don't need to know when it is fired.
+     *
+     * Added for Geneious API 4.701 (Geneious version 7.0.1)
+     */
+    public static final SimpleListener EMPTY = new SimpleListener() {
+        @Override
+        public void objectChanged() {
+            //do nothing, i'm empty!
+        }
+    };
 }
