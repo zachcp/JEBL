bec5ec364492d0c28eee7aa9ae72e9a379efdb7e
Matt Kearse <matt@biomatters.com>
2014-05-08 02:32:42 +0000

    GEN-5986 Core: Support Inosine nucleotide (I)?
    A few tests broke from the earlier changes. Most are just tests that need updating. However, there is some dodgy restriction analysis code that assumes gap_state is the last state in the list, so I've changed the index of inosine.
    
    git-svn-id: http://svn.code.sf.net/p/jebl/code/trunk@1180 6721810a-1124-0410-938e-bb277542c816

--- jebl/src/jebl/evolution/sequences/Nucleotides.java
+++ jebl/src/jebl/evolution/sequences/Nucleotides.java
@@ -50,9 +50,9 @@ public final class Nucleotides {
     public static final NucleotideState V_STATE = new NucleotideState("A/C/G", "V", 13, new NucleotideState[] {A_STATE, C_STATE, G_STATE});
     public static final NucleotideState N_STATE = new NucleotideState("Unknown base", "N", 14, new NucleotideState[] {A_STATE, C_STATE, G_STATE, T_STATE});
 
-    public static final NucleotideState UNKNOWN_STATE = new NucleotideState("Unknown base", "?", 15, new NucleotideState[] {A_STATE, C_STATE, G_STATE, T_STATE});
-    public static final NucleotideState GAP_STATE = new NucleotideState("Gap", "-", 16, new NucleotideState[] {A_STATE, C_STATE, G_STATE, T_STATE});
-    public static final NucleotideState I_STATE = new NucleotideState("Inosine", "I", 17, new NucleotideState[] {A_STATE, G_STATE, T_STATE}); // Inosine binds with A, C, U, so treat is as A, G, or T
+    public static final NucleotideState I_STATE = new NucleotideState("Inosine", "I", 15, new NucleotideState[] {A_STATE, G_STATE, T_STATE}); // Inosine binds with A, C, U, so treat is as A, G, or T
+    public static final NucleotideState UNKNOWN_STATE = new NucleotideState("Unknown base", "?", 16, new NucleotideState[] {A_STATE, C_STATE, G_STATE, T_STATE});
+    public static final NucleotideState GAP_STATE = new NucleotideState("Gap", "-", 17, new NucleotideState[] {A_STATE, C_STATE, G_STATE, T_STATE}); // Some code (e.g. restriction analysis) assumes GAP_STATE is the state with the highest index.
 
     // Making an array public allows a client to modify its contents. Deprecating on 2007-10-10
     // and will become private in the future. Use {@link #getCanonicalStates} instead.
@@ -68,7 +68,7 @@ public final class Nucleotides {
         A_STATE, C_STATE, G_STATE, T_STATE,
         R_STATE, Y_STATE, M_STATE, W_STATE,
         S_STATE, K_STATE, B_STATE, D_STATE,
-        H_STATE, V_STATE, N_STATE, UNKNOWN_STATE, GAP_STATE, I_STATE
+        H_STATE, V_STATE, N_STATE, I_STATE, UNKNOWN_STATE, GAP_STATE
     };
 
     // Making an array public allows a client to modify its contents. Deprecating on 2007-10-10
@@ -78,7 +78,8 @@ public final class Nucleotides {
             T_STATE, G_STATE, C_STATE, A_STATE,
             Y_STATE, R_STATE, K_STATE, W_STATE,
             S_STATE, M_STATE, V_STATE, H_STATE,
-            D_STATE, B_STATE, N_STATE, UNKNOWN_STATE, GAP_STATE, UNKNOWN_STATE // Not sure on this last one. Probably it doesn't matter since nobody should complement Inosine, but if they do, making it clear we don't know the answer seems best
+            D_STATE, B_STATE, N_STATE, UNKNOWN_STATE,  // Not sure on this last one. Probably it doesn't matter since nobody should complement Inosine, but if they do, making it clear we don't know the answer seems best
+            UNKNOWN_STATE, GAP_STATE
     };
 
     public static NucleotideState getComplementaryState(NucleotideState state) {
